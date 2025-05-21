6589d13c731c0a0f2f2c02da5fddcefa960aa74b
Micah Nyabuto <atembamicah@gmail.com>
2025-05-21 19:02:20 +0300

    Updated colors

--- app/src/main/java/com/micahnyabuto/thera/ui/theme/Color.kt
+++ app/src/main/java/com/micahnyabuto/thera/ui/theme/Color.kt
@@ -2,12 +2,24 @@ package com.micahnyabuto.thera.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+
+
+val PrimaryColor = Color(0xFF64B5F7)
+val PrimaryLightColor = PrimaryColor.copy(alpha = 0.75f)
+
 val SecondaryColor = Color(0xFF2196F3)
+val SecondaryLightColor = SecondaryColor.copy(alpha = 0.75f)
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryTextColor = Color(0xffffffff)
+val SecondaryTextColor = Color(0xff000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SurfaceDark = Color(0xff161616)
+
+val SurfaceLight = Color(0xfff5f5f5)
+
+val BackgroundLightColor = Color(0xfff5f5f5)
+
+val BackgroundDarkColor = Color(0xff101010)
+
+val ErrorColor = Color(0xfff44336)
+val OnErrorColor = Color(0xff000000)
