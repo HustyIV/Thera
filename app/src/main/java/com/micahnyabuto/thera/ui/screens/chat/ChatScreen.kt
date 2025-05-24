b976be21afe1ee4a1db76b2c40f28343390938ae
Micah Nyabuto <atembamicah@gmail.com>
2025-05-24 12:00:39 +0300

    Added ChatScreen

--- app/src/main/java/com/micahnyabuto/thera/ui/screens/chat/ChatScreen.kt
+++ app/src/main/java/com/micahnyabuto/thera/ui/screens/chat/ChatScreen.kt
@@ -1,2 +1,112 @@
 package com.micahnyabuto.thera.ui.screens.chat
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIos
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.micahnyabuto.thera.ui.navigation.Destinations
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ChatScreen(
+    navController: NavController
+){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Chat with Thera")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.navigate(Destinations.Home)
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBackIos,
+                            contentDescription = "Navigate up"
+                        )
+                    }
+                },
+//                colors = TopAppBarColors(
+//                    containerColor = MaterialTheme.colorScheme.primary,
+//                    scrolledContainerColor = Color.White,
+//                    navigationIconContentColor = Color.White,
+//                    titleContentColor = Color.White,
+//                    actionIconContentColor = Color.White
+//                )
+            )
+        },
+        bottomBar = {
+            BottomAppBar{
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {  },
+                        placeholder = { Text(text = "Start chat here...") },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Text
+                        ),
+                        modifier = Modifier
+                            .weight(0.5f)
+                            .padding(end = 8.dp),
+                        shape = RoundedCornerShape(80.dp),
+
+                        )
+
+                    IconButton(
+                        onClick = {
+
+                        },
+                        modifier = Modifier
+                            .size(48.dp)
+                            .background(
+                                color = MaterialTheme.colorScheme.primary,
+                                shape = CircleShape
+                            )
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Send,
+                            contentDescription = "Send",
+                            tint = Color.White
+                        )
+                    }
+                }
+            }
+        }
+    ) {
+
+
+    }
+}
+
