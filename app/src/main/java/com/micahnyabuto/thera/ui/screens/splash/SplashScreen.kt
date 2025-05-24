a23de238538ace80957ce6c461c15d0f8598222f
Micah Nyabuto <atembamicah@gmail.com>
2025-05-24 11:59:21 +0300

    Updated SplashScreen

--- app/src/main/java/com/micahnyabuto/thera/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/micahnyabuto/thera/ui/screens/splash/SplashScreen.kt
@@ -1,29 +1,21 @@
 package com.micahnyabuto.thera.ui.screens.splash
 
-import android.window.SplashScreenView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.Text
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.micahnyabuto.thera.R
 import com.micahnyabuto.thera.ui.theme.SecondaryColor
@@ -37,7 +29,6 @@ fun SplashScreen(
         delay(2000)
        onSplashFinished()
     }
-    Column {
         Box(
             modifier = Modifier.fillMaxSize()
                 .background(SecondaryColor),
@@ -53,29 +44,13 @@ fun SplashScreen(
             )
 
         }
-        Spacer(Modifier.height(200.dp))
-
-        Text("Welcome to Thera")
-    }
     Box(
         modifier = Modifier.fillMaxSize()
-            .padding(bottom = 200.dp),
+            .padding(bottom = 100.dp)
+            .fillMaxWidth(),
         contentAlignment = Alignment.BottomCenter
     ){
-
-        Text("Welcome to Thera")
+        CircularProgressIndicator()
     }
-
-
-    Box (
-        modifier = Modifier.fillMaxSize()
-            .fillMaxWidth(0.4f)
-            .padding(bottom = 100.dp),
-        contentAlignment = Alignment.BottomCenter
-    ){
-        LinearProgressIndicator()
-    }
-
-
 
 }
