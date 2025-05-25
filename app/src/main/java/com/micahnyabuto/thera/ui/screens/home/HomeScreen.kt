1612c9d57c96e526b5a9185f9b1a6bc22867ab60
Micah Nyabuto <atembamicah@gmail.com>
2025-05-25 20:23:18 +0300

    Update

--- app/src/main/java/com/micahnyabuto/thera/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/micahnyabuto/thera/ui/screens/home/HomeScreen.kt
@@ -3,24 +3,31 @@ package com.micahnyabuto.thera.ui.screens.home
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.micahnyabuto.thera.ui.navigation.Destinations
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -41,12 +48,29 @@ fun HomeScreen(
                             contentDescription = "notifications"
                         )
                     }
-                }
+                },
             )
-        }
+        },
+
+        floatingActionButton = {
+            OutlinedButton(
+                onClick = {navController.navigate(Destinations.Chat) },
+                shape = RoundedCornerShape(100.dp)
+            ) {
+                Row {
+                    Text("Start Chat")
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = null
+                    )
+                }
+            }
+
+
+        },
     ){innerpadding ->
         Card(
-            modifier = Modifier.padding(start = 10.dp,top= 100.dp, end = 10.dp)
+            modifier = Modifier.padding(innerpadding)
                 .clickable{navController.navigate(Destinations.Chat)}
                 .height(50.dp)
                 .fillMaxWidth(),
@@ -65,4 +89,11 @@ fun HomeScreen(
         }
 
     }
+}
+@Preview(showBackground =true )
+@Composable
+fun HomeScreenPreview(){
+    HomeScreen(
+        navController = rememberNavController()
+    )
 }
