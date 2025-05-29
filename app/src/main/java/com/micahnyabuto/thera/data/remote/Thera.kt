3063a502394b5c761f502266d1e956a927170f24
Micah Nyabuto <atembamicah@gmail.com>
2025-05-29 20:36:27 +0300

    Update

---
+++ app/src/main/java/com/micahnyabuto/thera/data/remote/Thera.kt
@@ -0,0 +1,9 @@
+package com.micahnyabuto.thera.data.remote
+
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface Thera {
+    @POST("chat")
+    suspend fun sendMessage(@Body request: ChatRequest): ChatResponse
+}
