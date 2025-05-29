fc1b0e71f71730384f3118470fd4330b5130846c
Micah Nyabuto <atembamicah@gmail.com>
2025-05-29 20:38:29 +0300

    Created the NewtworkModule

---
+++ app/src/main/java/com/micahnyabuto/thera/di/NetworkModule.kt
@@ -0,0 +1,30 @@
+package com.micahnyabuto.thera.di
+
+import com.micahnyabuto.thera.data.remote.Thera
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://therabothutsy.onrender.com/api/chat/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideThera(retrofit: Retrofit): Thera {
+        return retrofit.create(Thera::class.java)
+    }
+}
